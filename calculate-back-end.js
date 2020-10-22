628c6594c8fc67de45ea5a5369e83482c4bea8d4
Crystal K Young <maybiz@hotmail.com>
2020-10-21 20:47:30 -0400

    finished

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -2,8 +2,31 @@
  * YOUR CODE HERE *
  ******************/
 
+const calculate = function(num1, num2, operation) {
+  num1 = Number(num1)
+  num2 = Number(num2)
 
- 
+  const plus = operation === '+' || operation === 'plus' || operation === 'added to';
+  const minus = operation === '-' || operation === 'minus' || operation === 'subtracted from';
+  const times = operation === 'x' || operation === 'X' || operation === 'times';
+  const dividedBy = operation === '/' || operation === 'divided by';
+  const modBy= operation === '%' || operation === 'modulus' || operation === 'mod';
+
+  if (plus) {
+    return num1 + num2 
+  } else if (minus) {
+    return num1 - num2
+  } else if (times) {
+    return num1 * num2
+  } else if (dividedBy) {
+    return num1 / num2
+  } else if (modBy) {
+    return num1 % num2
+  } else {
+    return "Sorry, that's not a mathematical operation!"
+  }
+}
+
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
